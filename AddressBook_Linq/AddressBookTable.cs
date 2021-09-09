be7010614eb4a72477f01215f789a00fc42e3780
Rahul prabhu <rahul.prabu.07@gmail.com>
2021-09-09 11:41:56 +0530

    feat:Inserted new Contacts in AddressBook

--- AddressBook_Linq/AddressBookTable.cs
+++ AddressBook_Linq/AddressBookTable.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 using System.Text;
 
 namespace AddressBook_Linq
 {
     public class AddressBookTable
     {
-        public void CreateAddressBookDataTable()
+        public DataTable AddAddressBookDataTable()
         {
             DataTable table = new DataTable();
             table.Columns.Add("FirstName", typeof(string));
@@ -18,6 +19,25 @@ namespace AddressBook_Linq
             table.Columns.Add("Zip", typeof(int));
             table.Columns.Add("PhoneNumber", typeof(long));
             table.Columns.Add("Email", typeof(string));
+
+            table.Rows.Add("Tina", "Sharma", "House no 4", "TSK", "Assam", 786643, 997325546, "tina@gmail.com");
+            table.Rows.Add("Asha", "Das", "ITPL", "Bangalore", "Karnataka", 40002, 999000880, "asha@gmail.com");
+            table.Rows.Add("Prakash", "Baruah", "Panji", "Panaji", "Goa", 43254, 7777743210, "prakash@gmail.com");
+            table.Rows.Add("Rama", "Phukon", "Park street", "Kolkata", "West Bangal", 43254, 7877743990, "Rama@gmail.com");
+            table.Rows.Add("Rekha", "Chetia", "GH road", "Ghy", "Assam", 43254, 7888743210, "rekha@gmail.com");
+
+            return table;
+        }
+        public void DisplayContacts(DataTable addresstable)
+        {
+            var contacts = addresstable.Rows.Cast<DataRow>();
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine("\n------------------------------------");
+                Console.Write("First Name : " + contact.Field<string>("FirstName") + " " + "Last Name : " + contact.Field<string>("LastName") + " " + "Address : " + contact.Field<string>("Address") + " " + "City : " + contact.Field<string>("City") + " " + "State : " + contact.Field<string>("State")
+                    + " " + "Zip : " + contact.Field<int>("Zip") + " " + "Phone Number : " + contact.Field<long>("PhoneNumber") + " " + "Email : " + contact.Field<string>("Email") + " ");
+            }
         }
     }
+    
 }
--- AddressBook_Linq/Program.cs
+++ AddressBook_Linq/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 
 namespace AddressBook_Linq
 {
@@ -7,6 +8,9 @@ namespace AddressBook_Linq
         static void Main(string[] args)
         {
             Console.WriteLine("Address book program using Linq");
+            AddressBookTable addressBookTable = new AddressBookTable();
+            DataTable dataTable = addressBookTable.AddAddressBookDataTable();
+            addressBookTable.DisplayContacts(dataTable);
         }
     }
 }
